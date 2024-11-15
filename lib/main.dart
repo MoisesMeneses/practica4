48d12ee3f842ead7869a66f354dcc7a8d398a6f7
SciCristian <menesesjanckosocimocristian1@gmail.com>
2024-11-14 22:49:31 -0400

    cuarto comict

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/evento_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+runApp(
+    MultiProvider(
+    providers: [
+    ChangeNotifierProvider(create: (_) => EventProvider()),
+    ],
+    child: const MyApp(
+    ),
+    ),
+);
+}
+
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Gestión de Eventos',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: const Color.fromARGB(255, 216, 213, 253),
+            ),
+    home: HomeScreen(),
+    );
+}
+}
